623404b9cb289142bbdd58a9662a23c94bd91f9d
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-07-13 09:53:23 +0200

    Test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,22 @@
 require "bundler/setup"
 require "test_runner"
 
+unless ENV['SKIP_COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  )
+
+  SimpleCov.start do
+    add_filter 'spec'
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
