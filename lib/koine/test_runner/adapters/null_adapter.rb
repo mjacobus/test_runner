7ebd5a3cbf871cc6f0635544ccb73a80720b68ce
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-03-16 15:37:25 +0100

    Fix adapter

--- lib/koine/test_runner/adapters/null_adapter.rb
+++ lib/koine/test_runner/adapters/null_adapter.rb
@@ -2,10 +2,6 @@ module Koine
   class TestRunner
     class Adapters
       class NullAdapter
-        def accept?(_config)
-          true
-        end
-
         def test_command(_config)
           nil
         end
