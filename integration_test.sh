731d9cec674420749c9d38369644f78c333c5770
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-01-25 11:43:55 -0300

    Add integration test back to the game

--- integration_test.sh
+++ integration_test.sh
@@ -4,7 +4,7 @@ set -e
 set -v
 
 bundle exec rake install
-./exe/run_test spec/fixtures/FixturePhpUnitTest.php --line=30
+# ./exe/run_test spec/fixtures/FixturePhpUnitTest.php --line=30
 ./exe/run_test spec/koine/test_runner_spec.rb
 ./exe/run_test spec/koine/test_runner_spec.rb --line=9
 ./exe/run_test spec/koine/test_runner_spec.rb --all
