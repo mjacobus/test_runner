d3d4b196f470fbcb56d6888ebfdac2aa51f878e0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-01-25 11:48:16 -0300

    Fix configuration of integration tests

--- tests/integration_tests.sh
+++ tests/integration_tests.sh
@@ -5,7 +5,8 @@ set -v
 
 export SKIP_COVERAGE='true'
 
-./exe/run_test spec/fixtures/FixturePhpUnitTest.php --line=30
+bundle exec rake install
+# ./exe/run_test spec/fixtures/FixturePhpUnitTest.php --line=30
 ./exe/run_test spec/koine/test_runner_spec.rb
 ./exe/run_test spec/koine/test_runner_spec.rb --line=9
 ./exe/run_test spec/koine/test_runner_spec.rb --last
