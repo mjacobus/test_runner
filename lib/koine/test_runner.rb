fde5007a96b7e511e5f0a2bda2c744a81b9a55df
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-03-14 00:31:04 -0300

    Log log executions

--- lib/koine/test_runner.rb
+++ lib/koine/test_runner.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'logger'
 
 require 'koine/test_runner/version'
 require 'koine/test_runner/command_executer'
--- lib/koine/test_runner/command_executer.rb
+++ lib/koine/test_runner/command_executer.rb
@@ -2,7 +2,11 @@ module Koine
   class TestRunner
     class CommandExecuter
       def execute(command)
+        start = Time.now
         system(command.to_s)
+        finish = Time.now
+        elapsed = finish - start
+        logger.info("#{command} => elapsed time: #{elapsed.round(2)}")
         $?.exitstatus
       end
 
@@ -14,6 +18,10 @@ module Koine
       def execute_and_exit(command)
         puts command
         exit(execute(command))
+      end
+
+      def logger
+        @logger ||= Logger.new('/tmp/test_runner.log')
       end
     end
   end
